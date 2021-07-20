039a77abfab4c56ac62e1db6d30dbfdb5f784c6f
jlinoff <jlinoff@versive.com>
2021-07-20 08:53:37 -0700

    fix gdate ref for container

--- util.mk
+++ util.mk
@@ -1,9 +1,10 @@
 # Shared make utilities.
 # Handle systems like Mac that have really out of date versions of
 # standeard tools and have GNU tools installed on top.
-SED ?= $(shell gsed --version 2>/dev/null 1>/dev/null   && which gsed || which sed)
-SORT ?= $(shell gsort --version 2>/dev/null 1>/dev/null && which gsort || which sort)
-TAR ?= $(shell gnutar --version 2>/dev/null 1>/dev/null && which gnutar || which tar)
+GDATE ?= $(shell gdate --version 2>/dev/null 1>/dev/null && which gdate || which date)
+SED ?= $(shell gsed --version 2>/dev/null 1>/dev/null    && which gsed || which sed)
+SORT ?= $(shell gsort --version 2>/dev/null 1>/dev/null  && which gsort || which sort)
+TAR ?= $(shell gnutar --version 2>/dev/null 1>/dev/null  && which gnutar || which tar)
 EGREP ?= $(shell which grep) -E
 COLUMN ?= $(shell which column)
 
